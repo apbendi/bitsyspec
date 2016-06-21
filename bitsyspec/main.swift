3358132ccd135e4bcefb11aec6e447f8fa1393fd
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-06-21 16:42:27 -0400

    Add .gitignore

--- bitsyspec/main.swift
+++ bitsyspec/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  bitsyspec
-//
-//  Created by Ben DiFrancesco on 6/21/16.
-//  Copyright © 2016 ScopeLift. All rights reserved.
-//
-
 import Foundation
 
 print("Hello, World!")
