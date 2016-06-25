6de2cf6a632e72d0f3fcc64a30abec40c99a8055
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-06-25 15:23:54 -0400

    Include a description of the spec and produce better output when running it

--- bitsyspec/Spec.swift
+++ bitsyspec/Spec.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 struct Spec {
-
-    let expected: String
     let specPath: String
+    let expected: String
+    let description: String
 
     init?(filePath: String) {
         guard let regExp = try? NSRegularExpression(pattern: "^.+\\.bitsy$", options: .CaseInsensitive),
@@ -14,32 +14,32 @@ struct Spec {
         }
 
         guard let bitsyCode = try? NSString(contentsOfFile: filePath, encoding: NSUTF8StringEncoding) as String,
-            expectedOutput = Spec.extractExpected(fromCode: bitsyCode) else {
+            (description, expected) = Spec.extractExpected(fromCode: bitsyCode) else {
                 print("Could not determine expected output for spec at: \(filePath)")
                 return nil
         }
 
-
         self.specPath = filePath
-        self.expected = expectedOutput
+        self.expected = expected
+        self.description = description
     }
 
     func run(withBitsy bitsyBin: String) -> String {
         let output = Shell.exec(cmd: "\(bitsyBin) \(specPath)")
 
         if output == expected {
-            return "SUCCESS"
+            return "✅  \(description)"
         } else {
-            return "FAILURE"
+            return "❌  \(description) -> Expected '\(expected)' got '\(output)'".replacing(char: "\n", with: "\\n")
         }
     }
 }
 
 private extension Spec {
 
-    static let CommentLabel = "{Expected Output:\n"
+    static let CommentLabel = "{ Description: \""
 
-    static func extractExpected(fromCode code: String) -> String? {
+    static func extractExpected(fromCode code: String) -> (description: String, expected: String)? {
         var index = code.startIndex
 
         while index < CommentLabel.endIndex {
@@ -50,6 +50,19 @@ private extension Spec {
             index = index.successor()
         }
 
+        var description = ""
+
+        while code[index] != "\"" {
+            description += String(code[index])
+            index = index.successor()
+        }
+
+        index = index.successor()
+        guard code[index] == "\n" else {
+            return nil
+        }
+        index = index.successor()
+
         var expected = ""
 
         while code[index] != "}" {
@@ -57,6 +70,25 @@ private extension Spec {
             index = index.successor()
         }
         
-        return expected
+        return (description, expected)
+    }
+}
+
+private extension String {
+    func replacing(char char: Character, with replacement: String) -> String {
+        var index = self.startIndex
+        var new = ""
+
+        while index < self.endIndex {
+            if self[index] == char {
+                new += replacement
+            } else {
+                new += String(self[index])
+            }
+
+            index = index.successor()
+        }
+
+        return new
     }
 }
